1d23dd910dd0c3f47b3e663a32e334164bb5d0af
inf20001-byui <inf20001@byui.edu>
2022-01-16 12:32:48 -0500

    Development Code to Upload

---
+++ hilo/game/card.py
@@ -0,0 +1,32 @@
+# import the random library to genereate random numbers
+import random
+
+
+class Card:
+    """A card with a number from 1 to 13 on it.
+
+    The responsibility of Card is to keep track of the card drawn and calculate the points for 
+    it.
+   
+    Attributes:
+        value (int): The number on the card drawn.
+    """
+
+    def __init__(self):
+        """Constructs a new instance of Cards.
+
+        Args:
+            self (Cards): An instance of Cards.
+        """
+        self.value = 0
+        self.points = 0
+
+    def draw(self):
+        """Generates a new random value and calculates the points for the card drawn.
+        
+        Args:
+            self (Cards): An instance of a card.
+        """
+        # Still need to work on how points are calculated and awarded!
+        self.value = random.randint(1, 13)
+        self.points = 100 if self.value == 5 else 100 if self.value == 1 else 0
---
+++ hilo/game/director.py
@@ -0,0 +1,105 @@
+from game.card import Card
+from game.format import color
+
+
+class Director:
+    """
+    The responsibility of a Director is to control the sequence of play.
+
+    Attributes:
+        cards (List[Cards]): A list of Cards instances.
+        is_playing (boolean): Whether or not the game is being played.
+        score (int): The score for one round of play.
+        total_score (int): The score for the entire game.
+    """
+
+    def __init__(self):
+        """Constructs a new Director.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self.cards = []
+        self.is_playing = True
+        self.score = 0
+        self.total_score = 300
+        card = Card()
+        self.cards.append(card)
+
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.is_playing:
+            self.do_initial()
+            self.do_updates()
+            self.get_inputs()
+            self.do_outputs()
+
+    def get_inputs(self):
+        """Ask the user if they want to draw a card.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        draw_card = input("Is the next card " + color.BOLD + "Higher" + color.END + " or " + color.BOLD + "Lower? [h/l] ")
+        
+        
+       
+    def do_updates(self):
+        """Updates the player's score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if not self.is_playing:
+            return 
+      
+        for i in range(len(self.cards)):
+           card = self.cards[i]
+           card.draw()
+           self.score += card.points 
+        self.total_score += self.score
+
+    def do_outputs(self):
+        """Displays the card and the score. Also asks the player if they want to draw again. 
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if not self.is_playing:
+            return
+        
+        values = ""
+        for i in range(len(self.cards)):
+            card = self.cards[i]
+            values += f"{card.value} "
+
+        print("The next card is a " + color.RED + f"{values}" + color.END)
+        print("Your score is: " + color.BLUE + f"{self.total_score} \n" + color.END)
+        self.is_playing == (self.score > 0)
+        play_again = input("Would you like to play again? (y/n) ")
+        self.is_playing = (play_again == "y")
+    
+    def do_initial(self):
+        """Does the initial setup of the game and draws the first card. 
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if not self.is_playing:
+            return
+
+        for i in range(len(self.cards)):
+           card = self.cards[i]
+           card.draw()
+
+        values = ""
+        for i in range(len(self.cards)):
+            card = self.cards[i]
+            values += f"{card.value} "
+
+        print("Your card is a " + color.RED + f"{values}" + color.END)
+        self.is_playing == (self.score > 0)
