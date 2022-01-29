268cbf7b8539dd01f0fa540c5d04f84be0a96366
nahyraviana <nahyraviana@gmail.com>
2022-01-29 02:23:48 -0700

    update

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -5,9 +5,9 @@ import random
 class Card:
     """A card with a number from 1 to 13 on it.
 
-    The responsibility of Card is to keep track of the card drawn and calculate the points for 
+    The responsibility of Card is to keep track of the card drawn and calculate the points for
     it.
-   
+
     Attributes:
         value (int): The number on the card drawn.
     """
@@ -23,10 +23,12 @@ class Card:
 
     def draw(self):
         """Generates a new random value and calculates the points for the card drawn.
-        
+
         Args:
             self (Cards): An instance of a card.
         """
         # Still need to work on how points are calculated and awarded!
         self.value = random.randint(1, 13)
+
         self.points = 100 if True else -75
+
--- hilo/game/director.py
+++ hilo/game/director.py
@@ -15,7 +15,7 @@ class Director:
 
     def __init__(self):
         """Constructs a new Director.
-        
+
         Args:
             self (Director): an instance of Director.
         """
@@ -26,9 +26,12 @@ class Director:
         card = Card()
         self.cards.append(card)
 
+
+
+
     def start_game(self):
         """Starts the game by running the main game loop.
-        
+
         Args:
             self (Director): an instance of Director.
         """
@@ -38,6 +41,8 @@ class Director:
             self.get_inputs()
             self.do_outputs()
 
+
+
     def get_inputs(self):
         """Ask the user if they want to draw a card.
 
@@ -45,9 +50,26 @@ class Director:
             self (Director): An instance of Director.
         """
         draw_card = input("Is the next card " + color.BOLD + "Higher" + color.END + " or " + color.BOLD + "Lower? [h/l] ")
-        
-        
-       
+        return draw_card
+
+    #this is the function i'm working at.
+    #I'm trying to assign a value to the user's input, so we can use if statements to
+    #decide the scores.
+    def get_score(self):
+        self.guess = ""
+        if self.first_value < self.second_value:
+            self.guess.lower() == "h"
+        elif self.first_value > self.second_value:
+            self.guess.lower() == "l"
+        else:
+            print("Please type H or L")
+
+        if self.get_inputs.lower() == self.guess:
+            self.score = 100
+        elif self.get_inputs.lower() != self.guess:
+            self.score = -75
+
+
     def do_updates(self):
         """Updates the player's score.
 
@@ -55,36 +77,57 @@ class Director:
             self (Director): An instance of Director.
         """
         if not self.is_playing:
-            return 
-      
+            return
+
         for i in range(len(self.cards)):
            card = self.cards[i]
            card.draw()
-           self.score = card.points 
+           self.score = card.points
         self.total_score += self.score
 
+
     def do_outputs(self):
-        """Displays the card and the score. Also asks the player if they want to draw again. 
+        """Displays the card and the score. Also asks the player if they want to draw again.
 
         Args:
             self (Director): An instance of Director.
         """
         if not self.is_playing:
             return
+
         
-        values = ""
+        self.second_value = 0
         for i in range(len(self.cards)):
             card = self.cards[i]
-            values += f"{card.value} "
+            self.second_value += card.value
 
-        print("The next card is a " + color.RED + f"{values}" + color.END)
+
+        print()
+        print("The next card was " + color.RED + f"{self.second_value}" + color.END)
+
+        #I added a score message to the user knows if the guess was right or not
+        if self.score == 100:
+            print("Great! You've earned 100 Points")
+        elif self.score == -75:
+            print("Too bad! You've lost 75 Points")
+
         print("Your score is: " + color.BLUE + f"{self.total_score} \n" + color.END)
-        self.is_playing == (self.score > 0)
+
+        self.is_playing == (self.total_score > 0)
         play_again = input("Would you like to play again? (y/n) ")
+        print()
         self.is_playing = (play_again == "y")
-    
+        #I added this if Statement so if the user decides to leave the game it shows the final score and a thank you message
+        if play_again == "n":
+            print()
+            print("Your final score is: " + color.BLUE + f"{self.total_score} \n" + color.END)
+            print()
+            print("Thank you for playing!")
+        
+
+
     def do_initial(self):
-        """Does the initial setup of the game and draws the first card. 
+        """Does the initial setup of the game and draws the first card.
 
         Args:
             self (Director): An instance of Director.
@@ -96,10 +139,15 @@ class Director:
            card = self.cards[i]
            card.draw()
 
-        values = ""
+        self.first_value = 0 
         for i in range(len(self.cards)):
             card = self.cards[i]
-            values += f"{card.value} "
+            self.first_value += card.value
 
-        print("Your card is a " + color.RED + f"{values}" + color.END)
-        self.is_playing == (self.score > 0)
+        print("The card is " + color.RED + f"{self.first_value}" + color.END)
+
+        while self.score > 0:
+            self.is_playing = True
+        
+
+
